98ee6b72a53521e1f7f3128e91530702581253b2
USER-PC\USER <djdjejh@naver.com>
2018-03-15 20:11:22 +0900

    rest클래스 기능 구현 진행중

--- mini_jokiyo/src/kr/co/jokiyo/domain/Rest.java
+++ mini_jokiyo/src/kr/co/jokiyo/domain/Rest.java
@@ -4,10 +4,39 @@ public class Rest {
 	private int restId;
 	private String name;
 	private int typeId;
+	private char typeName;
 	private int exitNum;
-	private long tel;
+	private String tel;
 	private String openDate;
 	private String closeDate;
+	private String addr;
+	private int star;
+	private int reviewCnt;
+
+	public char getTypeName() {
+		return typeName;
+	}
+	public void setTypeName(char typeName) {
+		this.typeName = typeName;
+	}
+	public int getStar() {
+		return star;
+	}
+	public void setStar(int star) {
+		this.star = star;
+	}
+	public int getReviewCnt() {
+		return reviewCnt;
+	}
+	public void setReviewCnt(int reviewCnt) {
+		this.reviewCnt = reviewCnt;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
 	public int getRestId() {
 		return restId;
 	}
--- mini_jokiyo/src/kr/co/jokiyo/rest/ui/DetailReviewUI.java
+++ mini_jokiyo/src/kr/co/jokiyo/rest/ui/DetailReviewUI.java
@@ -1,5 +1,62 @@
 package kr.co.jokiyo.rest.ui;
 
-public class DetailReviewUI {
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import common.db.MyAppSqlConfig;
+import kr.co.jokiyo.common.ui.BaseUI;
+import kr.co.jokiyo.domain.Review;
+import kr.co.jokiyo.mapper.ListReviewMapper;
+
+public class DetailReviewUI extends BaseUI {
+	int num;
+	public ListReviewMapper mapper;
+	
+	
+	public DetailReviewUI(int num) {
+		SqlSession session = MyAppSqlConfig.getSqlSession();
+		mapper = session.getMapper(ListReviewMapper.class);
+		this.num = num;
+
+	}
+	public void service() { 
+		
+		System.out.println("================================");
+		List<Review> list = mapper.detailReview(num);
+		for(Review r: list) {
+		System.out.println("================================");
+		System.out.println(" 작성자 id : " + r.getId() );
+		System.out.println(" 리뷰번호 : " + r.getReviewId());
+		System.out.println(" 작성일자 : " + r.getRegDate() );
+		System.out.println(" 별점 : " + r.getStar());
+		System.out.println(" 내용 : " + r.getCmt() );
+		System.out.println(" 좋아요(갯수) :" +r.getGood());
+		System.out.println("================================");
+		}
+		/*
+		 * 상세보기 번호 입력시
+리뷰상세화면
+===========
+식당명
+------
+id
+리뷰번호
+작성일자.
+별점
+내용
+좋아요(개수)
+--------
+.좋아요(리뷰번호 입력)
+0번.뒤로가기
+===========
+
+		 * 
+		 * */
+		
+	}
 }
+
+	
+
