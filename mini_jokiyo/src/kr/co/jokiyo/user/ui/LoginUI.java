838c22c786685b9a9de484c0b7eb88cb28830cec
USER-PC\USER <raye09@naver.com>
2018-03-15 19:36:34 +0900

    UserUI 및 하위 클래스인 JoinUI, LoginUI, LoginFailUI, FindPwUI 클래스 완성

--- mini_jokiyo/src/kr/co/jokiyo/user/ui/LoginUI.java
+++ mini_jokiyo/src/kr/co/jokiyo/user/ui/LoginUI.java
@@ -1,5 +1,54 @@
 package kr.co.jokiyo.user.ui;
 
-public class LoginUI {
+import org.apache.ibatis.session.SqlSession;
+
+import common.db.MyAppSqlConfig;
+import kr.co.jokiyo.common.ui.BaseUI;
+import kr.co.jokiyo.domain.User;
+import kr.co.jokiyo.mapper.LoginMapper;
+
+public class LoginUI extends BaseUI {
+	BaseUI ui = null;
+	private LoginMapper mapper;
+	static String logUser;
+
+	public LoginUI() {
+		SqlSession session = MyAppSqlConfig.getSqlSession();
+		mapper = session.getMapper(LoginMapper.class);
+	}
+
+	@Override
+	public void service() throws Exception {
+		System.out.println("================================");
+		String id = getStr("아이디: ");
+		String pw = getStr("비밀번호: ");
+		User param = new User();
+		param.setId(id);
+		param.setPw(pw);
+
+		param = mapper.selectUserLogin(param);
+		if (param == null) {
+			System.out.println("로그인에 실패하였습니다.");
+			ui = new LoginFailUI();
+			ui.service();
+			return;
+		} 
+		
+		char chk = param.getAuthority();
+		
+		if (chk == '1') {
+			System.out.println("로그인에 성공하였습니다.");
+			System.out.println("================================");
+			logUser = param.getId();
+//			 SearchMainUI ui = new SearchMainUI();
+//			 ui.service();
+			
+		} else if (chk == '2') {
+			System.out.println("관리자 계정 로그인");
+			System.out.println("================================");
+//			 AdminUI admin = new AdminUI();
+//			 admin.service();
+		}
+	}
 
 }
